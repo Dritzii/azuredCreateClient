095204578ccfb89171e845aae8ba7b26d9aa1227
John <john@azured.com.au>
2022-03-22 14:32:12 +1100

    add string trimmer

---
+++ TrimStringFromUrl.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace azuredCreateClient
+{
+    class TrimStringFromUrl
+    {
+        string urlString;
+
+        public TrimStringFromUrl(string urlString)
+        {
+            this.urlString = urlString;
+        }
+
+        public string returnCode()
+        {
+            // Create a pattern for a word that starts with letter "M"  
+            string pattern = @"code=(.*?)&state=";
+            char[] charsToTrimStart = { 'c', 'o', 'd', 'e', '=' };
+            char[] charsToTrimEnd = { '&', 's', 't', 'a', 't', 'e', '=' };
+            // Create a Regex  
+            Regex rg = new Regex(pattern);
+
+            MatchCollection matchedAuthors = rg.Matches(this.urlString);
+            Console.WriteLine(matchedAuthors.Count);
+            string initialCode = matchedAuthors[0].ToString().TrimStart(charsToTrimStart).TrimEnd(charsToTrimEnd);
+
+            return initialCode;
+        }
+    }
+}
---
+++ trimCode.cs
@@ -0,0 +1,29 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace azuredCreateClient
+{
+    public static class trimCode
+    {
+        [FunctionName("trimCode")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            string authCode = data.code;
+            Console.WriteLine(authCode);
+            TrimStringFromUrl urlString = new TrimStringFromUrl(authCode);
+            string responseMessage = urlString.returnCode();
+            return new OkObjectResult(responseMessage);
+        }
+    }
+}
