df0e0cfae8f7f290e7d8ef4cf6d850a6414bf621
John <john@azured.com.au>
2022-03-23 04:41:22 +1100

    added microsoft graph functions

---
+++ MicrosoftGraph.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using RestSharp;
+
+namespace azuredCreateClient
+{
+    class MicrosoftGraph
+    {
+        readonly RestClient _client;
+        string Token;
+
+        public MicrosoftGraph(string Token)
+        {
+            _client = new RestClient();
+            this.Token = Token;
+        }
+
+        public async Task<string> GetMe()
+        {
+            var request = new RestRequest("https://graph.microsoft.com/v1.0/me");
+            request.AddHeader("Authorization", "Bearer " + Token);
+            var response = await _client.GetAsync(request);
+            Console.WriteLine(response.StatusCode);
+            Console.WriteLine(response.Content);
+            var DeserialresponseObject = JsonConvert.DeserializeObject(response.Content);
+            return (string)DeserialresponseObject;
+        }
+    }
+}
