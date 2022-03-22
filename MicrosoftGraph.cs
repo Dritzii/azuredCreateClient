f4232f937bc11d2b77f2df4962ef04d1c7df4e34
John <john@azured.com.au>
2022-03-23 09:57:54 +1100

    redone msgraph

--- MicrosoftGraph.cs
+++ MicrosoftGraph.cs
@@ -1,30 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
-using RestSharp;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace azuredCreateClient
 {
     class MicrosoftGraph
     {
-        readonly RestClient _client;
+        private static readonly HttpClient httpClient = new HttpClient();
         string Token;
 
         public MicrosoftGraph(string Token)
         {
-            _client = new RestClient();
             this.Token = Token;
         }
 
         public async Task<string> GetMe()
         {
-            var request = new RestRequest("https://graph.microsoft.com/v1.0/me");
-            request.AddHeader("Authorization", "Bearer " + Token);
-            var response = await _client.GetAsync(request);
-            Console.WriteLine(response.StatusCode);
-            Console.WriteLine(response.Content);
-            var DeserialresponseObject = JsonConvert.DeserializeObject(response.Content);
-            return (string)DeserialresponseObject;
+            var client = new HttpClient();
+            client.DefaultRequestHeaders.Add("Authorization", "Bearer " + this.Token);
+            string requestUrl = "https://graph.microsoft.com/v1.0/me";
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, requestUrl);
+
+
+            HttpResponseMessage response = await client.SendAsync(request);
+
+            string responseContent = await response.Content.ReadAsStringAsync();
+            dynamic responseObject = JsonConvert.DeserializeObject(responseContent);
+            return responseObject;
         }
     }
 }
--- MicrosoftGraphTrigger.cs
+++ MicrosoftGraphTrigger.cs
@@ -14,20 +14,17 @@ namespace azuredCreateClient
     {
         [FunctionName("MicrosoftGraph")]
         public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequest req,
             ILogger log)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
-
-            string name = req.Query["name"];
-
+            // Get the authentication code from the request payload
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
             dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
-
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+            string authCode = data.code;
+            Console.WriteLine(authCode);
+            MicrosoftGraph graphCall = new MicrosoftGraph(authCode);
+            var responseMessage = graphCall.GetMe();
+            
 
             return new OkObjectResult(responseMessage);
         }
