0996beb75ef9ee025213514fc60ed6db7ac15851
John <john@azured.com.au>
2022-03-27 14:26:45 +1100

    step

--- SetRbacSubscriptions.cs
+++ SetRbacSubscriptions.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 
@@ -27,21 +28,20 @@ namespace azuredCreateClient
             using var client = new HttpClient();
             string joinedURL = hostUrl + subscriptionId + endpointUrl + rbacName + apiVersion;
             client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", this.Token);
-            var bottomObject = new { roleDefinitionId = "/subscriptions/" + subscriptionId + "providers/Microsoft.Authorization/roleDefinitions/8e3af657-a8ff-443c-a75c-2fe8c4bcb635", principalId = principalId };
+            var bottomObject = new { roleDefinitionId = "/subscriptions/" + subscriptionId + "/providers/Microsoft.Authorization/roleDefinitions/8e3af657-a8ff-443c-a75c-2fe8c4bcb635", principalId = principalId };
             Console.WriteLine(bottomObject);
             var topObject = new { properties = bottomObject };
             Console.WriteLine(topObject);
             var jsonToReturn = JsonConvert.SerializeObject(topObject);
             Console.WriteLine(jsonToReturn);
-            //client.DefaultRequestHeaders.Add("Content-Type", "application/json");
-            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Put, joinedURL);
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Put, joinedURL) {
+                Content = new StringContent(jsonToReturn.ToString(), Encoding.UTF8, "application/json"),
+            };
             HttpResponseMessage response = await client.SendAsync(request);
             string responseContent = await response.Content.ReadAsStringAsync();
             dynamic responseObject = JsonConvert.DeserializeObject(responseContent);
             Console.WriteLine(responseContent);
             Console.WriteLine(responseObject);
-            //return responseObject;
-
         }
     }
 }
