65e0f30ad4ff33d2d0778b530374a352776927e9
John <john@azured.com.au>
2022-03-28 19:03:49 +1100

    step

--- GetSubscriptions.cs
+++ GetSubscriptions.cs
@@ -39,6 +39,28 @@ namespace azuredCreateClient
 
 
         }
+        public async Task<string> GetAllSubscriptionsGraph()
+        {
+            Console.WriteLine(hostUrl);
+            JsonSerializerSettings jss = new JsonSerializerSettings();
+            jss.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+            using var client = new HttpClient();
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", this.Token);
+            //client.DefaultRequestHeaders.Add("Content-Type", "application/json");
+            string hostUrltest = "https://graph.microsoft.com/v1.0/subscriptions";
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, hostUrltest);
+            Console.WriteLine(hostUrl);
+            HttpResponseMessage response = await client.SendAsync(request);
+            string responseContent = await response.Content.ReadAsStringAsync();
+            //dynamic responseObject = JsonConvert.DeserializeObject(responseContent);
+            Console.WriteLine(responseContent);
+            var jo = JObject.Parse(responseContent);
+            string subid = jo["value"][0]["subscriptionId"].ToString();
+            Console.WriteLine("subscription id " + subid);
+            return subid;
+
+
+        }
 
     }
 }
--- MicrosoftIdentityClient.cs
+++ MicrosoftIdentityClient.cs
@@ -68,5 +68,51 @@ namespace azuredCreateClient
                 throw new Exception("Something bad happened");
             }
         }
+
+        public async Task<string> GetAccessTokenFromAuthorizationCodeClientCredentials(string authCode, string tenantId)
+        {
+            string redirectUrl = Environment.GetEnvironmentVariable("redirecturi", EnvironmentVariableTarget.Process);
+            string scopes = "openid offline_access https://graph.microsoft.com/.default";
+
+            Uri requestUri = new Uri($"{hostUrl}/{tenantId}/oauth2/v2.0/token");
+
+            List<KeyValuePair<string, string>> content = new List<KeyValuePair<string, string>>()
+            {
+                new KeyValuePair<string, string>("client_id", this.clientId),
+                new KeyValuePair<string, string>("scope", scopes),
+                new KeyValuePair<string, string>("grant_type", "client_credentials"),
+                new KeyValuePair<string, string>("code", authCode),
+                new KeyValuePair<string, string>("redirect_uri", redirectUrl),
+                new KeyValuePair<string, string>("client_secret", this.clientSecret)
+                //new KeyValuePair<string, string>("resource", "https://management.azure.com")
+            };
+
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, requestUri)
+            {
+                Content = new FormUrlEncodedContent(content),
+            };
+
+            HttpResponseMessage response = await httpClient.SendAsync(request);
+
+            string responseContent = await response.Content.ReadAsStringAsync();
+            dynamic responseObject = JsonConvert.DeserializeObject(responseContent);
+
+            if (response.IsSuccessStatusCode)
+            {
+                // dynamic values need to be assigned before passing back
+                return (string)responseObject.access_token;
+            }
+            else if (response.StatusCode == HttpStatusCode.BadRequest)
+            {
+                // Something failed along the way, and there will be an error in there if the error code is 400
+                // Handle it however you want.
+                throw new Exception((string)responseObject.error_description);
+            }
+            else
+            {
+                // ¯\_(ツ)_/¯
+                throw new Exception("Something bad happened");
+            }
+        }
     }
 }
