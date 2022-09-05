e3ba112b4ba4b2ec9ea1aee524aee063b5469230
John <john@azured.com.au>
2022-09-06 03:02:05 +1000

    added swagger ui

---
+++ SwaggerFunctions.cs
@@ -0,0 +1,33 @@
+﻿using AzureFunctions.Extensions.Swashbuckle.Attribute;
+using AzureFunctions.Extensions.Swashbuckle;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace azuredCreateClient
+{
+    public static class SwaggerFunctions
+    {
+        [SwaggerIgnore]
+        [FunctionName("Swagger")]
+        public static Task<HttpResponseMessage> Swagger(
+                [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "swagger/json")] HttpRequestMessage req,
+                [SwashBuckleClient] ISwashBuckleClient swasBuckleClient)
+        {
+            return Task.FromResult(swasBuckleClient.CreateSwaggerJsonDocumentResponse(req));
+        }
+        [SwaggerIgnore]
+        [FunctionName("SwaggerUI")]
+        public static Task<HttpResponseMessage> SwaggerUI(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "swagger/ui")] HttpRequestMessage req,
+        [SwashBuckleClient] ISwashBuckleClient swasBuckleClient)
+        {
+            return Task.FromResult(swasBuckleClient.CreateSwaggerUIResponse(req, "swagger/json"));
+        }
+    }
+}
